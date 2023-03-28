c1b227645ccfdb49707dd8ddb2e691757f07d816
Muhammet <muhammetsarican@gmail.com>
2023-03-28 17:54:20 +0300

    The method added that takes image as parts, some adjusments added

--- ImageSection.py
+++ ImageSection.py
@@ -1,15 +1,18 @@
 import cv2, os, json, keyboard
 from threading import Thread
+import pyautogui
 
 with open("filePath.json", "r", encoding="utf-8") as file:
     paths=json.load(file)
-
+    
 paths=paths["win_path"]
+
 class ImageSection:
     isUiFirstLoad = True
     frameLocationsList = []
     
     frame = cv2.imread(paths["image_path"])
+    # frame=cv2.resize(frame, None, fx=0, fy=0)
 
     signedFrame=frame.copy()
     notSignedFrame = frame.copy()
@@ -20,22 +23,39 @@ class ImageSection:
     width = frame.shape[1]
     height = frame.shape[0]
     
+    def getParts(self):
+        return int(self.frame.shape[1]/pyautogui.size()[0]), int(self.frame.shape[0]/pyautogui.size()[1])# It sends width first, height second
+    
+    def fileCounter(self):
+        fileCounter=0
+        for path in os.listdir(paths["image_save_path"]):
+            for _ in os.listdir(paths["image_save_path"]+path):
+                fileCounter+=1
+        return fileCounter
+    
+    def radiobutton_event(self, ui):
+        print("Height radiobutton toggled, current value:", ui.heightVar.get())
+        print("Width radiobutton toggled, current value:", ui.widthVar.get())
+        
     upsideORdownside=True
-    def getBaseImage(self):
-        if self.upsideORdownside:
-            height_start=int(0)
-            height_end=int(self.height/2)
-        else:
-            height_start=int(self.height/2)
-            height_end=int(self.height)
-            
-        frame = self.signedFrame[height_start:height_end, 0:self.width, :]
-        return frame, height_start, height_end    
+    width_start=0
+    height_start=0  
+    
+    def getBaseImage(self, ui=None):
+        if ui is not None:
+            self.width_start=ui.widthVar.get()*pyautogui.size()[0]
+            self.height_start=ui.heightVar.get()*pyautogui.size()[1]
+            print(f"Ui: {self.width_start, self.height_start}")
+        
+        width_end=self.width_start+pyautogui.size()[0]
+        height_end=self.height_start+pyautogui.size()[1]
+
+        frame = self.signedFrame[self.height_start:height_end, self.width_start:width_end, :]
+        return frame, self.height_start, height_end, self.width_start, width_end
 
     def __init__(self) -> None:
         pass
     def start(self):
-        print("start")
         self.takeFrameThread = Thread(target=self.takeFrame, args=())
         self.takeFrameThread.daemon=True
         self.takeFrameThread.start()
@@ -47,19 +67,15 @@ class ImageSection:
         for item in self.frameLocationsList:
             if item["source_image_path"] == paths["image_path"]:
                 if os.path.exists(item["resized_image_path"]):
-                    cv2.putText(self.signedFrame, item["resized_image_path"].split("/")[-2], (item["start_x"]+5, item["start_y"]+25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+                    cv2.putText(self.signedFrame, item["resized_image_path"].split("/")[-2], (item["start_x"]+5, item["start_y"]+75), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 2)
                     cv2.rectangle(self.signedFrame, (item["start_x"], item["start_y"]),
-                                  (item["end_x"], item["end_y"]), (100, 0, 255), 2)
+                                  (item["end_x"], item["end_y"]), (0, 100, 255), 2)
 
     def saveResizedImage(self, choice, root):
-        _, height_start, height_end=self.getBaseImage()
+        _, height_start, height_end, width_start, width_end=self.getBaseImage()
         # print(data_path+choice.get())
         # for path in os.listdir(data_path+choice.get()):
         #     print(path)
-        with open('imageNameCounter.txt', 'r', encoding='UTF-8') as file:
-            counter = int(file.readline())
-        with open('imageNameCounter.txt', 'w', encoding='UTF-8') as file:
-            file.write(str(counter+1))
         # print(self.bbox[1], self.bbox[1]+frame_height, self.bbox[0], self.bbox[0]+frame_width)
 
         # with open('frameLocations.json', "r", encoding='utf-8') as file:
@@ -70,30 +86,30 @@ class ImageSection:
         # print(self.frameLocationsList)
         frameLocation = {
             "source_image_path": paths["image_path"],
-            "resized_image_path": f"{paths['image_save_path']+choice.get()}/{counter}.jpg",
-            "start_x": self.bbox[0],
+            "resized_image_path": f"{paths['image_save_path']+choice.get()}/{self.fileCounter()}.jpg",
+            "start_x": self.bbox[0]+width_start,
             "start_y": self.bbox[1]+height_start,
-            "end_x": self.bbox[0]+self.frame_width,
-            "end_y": self.bbox[1]+self.frame_height+height_start
+            "end_x": self.bbox[0]+self.bbox[2]+width_start,
+            "end_y": self.bbox[1]+self.bbox[3]+height_start
         }
         self.frameLocationsList.append(frameLocation)
         with open('frameLocations.json', 'w', encoding='UTF-8') as file:
             json.dump(self.frameLocationsList, file,
                       ensure_ascii=False, indent=4)
-            # file.write(f"source_image_path={path},resized_image_path={data_path+choice.get()}/{counter}.jpg,start_x={self.bbox[0]},start_y={self.bbox[1]},end_x={self.bbox[0]+frame_width},end_y={self.bbox[1]+frame_height}\n")
+            # file.write(f"source_image_path={path},resized_image_path={data_path+choice.get()}/{self.fileCounter()}.jpg,start_x={self.bbox[0]},start_y={self.bbox[1]},end_x={self.bbox[0]+frame_width},end_y={self.bbox[1]+frame_height}\n")
 
         cv2.rectangle(self.frame, (self.bbox[0], self.bbox[1]), (
             self.bbox[0]+self.bbox[2], self.bbox[1]+self.bbox[3]), (0, 255, 0), 1)
         cv2.putText(self.frame, choice.get(
         ), (self.bbox[0]+5, self.bbox[1]+25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
-        cv2.imwrite(f"{paths['image_save_path']+choice.get()}/{counter}.jpg", self.image)
+        cv2.imwrite(f"{paths['image_save_path']+choice.get()}/{self.fileCounter()}.jpg", self.image)
         # cv2.destroyAllWindows()
         # takeFrameThread.kill()
         self.start()
     # print(f"{int(self.bbox[1])}:{int(self.bbox[1]+self.bbox[3])},{int(self.bbox[0])}:{int(self.bbox[0]+self.bbox[2])}")
 
     def takeFrame(self):
-        frame, height_start, height_end=self.getBaseImage()
+        frame, height_start, height_end, width_start, width_end=self.getBaseImage()
         self.findMarkedImages()                 
         self.bbox = cv2.selectROI(frame)
         cv2.destroyAllWindows()
@@ -101,9 +117,9 @@ class ImageSection:
         # self.takeFrameThread.cancel()
         # cv2.destroyAllWindows()
         if(self.bbox[2]<50 or self.bbox[3]<60):
-            self.image = self.notSignedFrame[int(self.bbox[1])+height_start:int(self.bbox[1])+self.frame_height+height_start, int(self.bbox[0]):int(self.bbox[0]+self.frame_width), :] # here I added the height_start because if I want to take downside of photo I have to add this but if I dont do this resized image will be upside of photo
+            self.image = self.notSignedFrame[int(self.bbox[1])+height_start:int(self.bbox[1])+self.frame_height+height_start, int(self.bbox[0])+width_start:int(self.bbox[0]+self.frame_width+width_start), :] # here I added the height_start because if I want to take downside of photo I have to add this but if I dont do this resized image will be upside of photo
         else:
-            self.image = self.notSignedFrame[int(self.bbox[1])+height_start:int(self.bbox[1])+self.bbox[3]+height_start, int(self.bbox[0]):int(self.bbox[0]+self.bbox[2]), :] # here I added the height_start because if I want to take downside of photo I have to add this but if I dont do this resized image will be upside of photo
+            self.image = self.notSignedFrame[int(self.bbox[1])+height_start:int(self.bbox[1])+self.bbox[3]+height_start, int(self.bbox[0])+width_start:int(self.bbox[0]+self.bbox[2]+width_start), :] # here I added the height_start because if I want to take downside of photo I have to add this but if I dont do this resized image will be upside of photo
             self.image=cv2.resize(self.image, (self.frame_width, self.frame_height))
         # cv2.imshow("notSignedFrame", self.image)
         # cv2.waitKey(0)
@@ -118,3 +134,7 @@ class ImageSection:
                 pass
         except:
             keyboard.press_and_release("c")
+
+        r_frame=cv2.resize(self.signedFrame, (pyautogui.size()[0]-100, pyautogui.size()[1]-100))
+        cv2.imshow("frame", r_frame)
+        cv2.waitKey(0)
--- uiCreate.py
+++ uiCreate.py
@@ -3,45 +3,72 @@ import customtkinter, keyboard, json
 class UI(customtkinter.CTk):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.geometry("400x600+0+0")
+        self.geometry(f"700x{self.winfo_screenheight()-50}+0+0")
         
-        self.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
-        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
+        self.grid_columnconfigure((0, 1, 2, 3, 4, 5), weight=1)
     
     def home(self, imageself):
         self.paths_button=customtkinter.CTkButton(self, text="Edit Paths", command=self.editPaths)
-        self.paths_button.grid(row=0, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
+        self.paths_button.grid(row=0, column=0, columnspan=6, padx=10, pady=5, sticky="ew")
         
         self.combobox = customtkinter.CTkOptionMenu(master=self,
-                                       values=[
-                                           "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-                                       )
-        self.combobox.grid(row=1, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
+                                    values=[
+                                        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+                                    )
+        self.combobox.grid(row=1, column=0, columnspan=6, padx=10, pady=5, sticky="ew")
         self.combobox.set("0")  # set initial value
 
-        self.submit = customtkinter.CTkButton(self, text="Save",
+        self.submit = customtkinter.CTkButton(self, text="Save", fg_color="green",
                                         command=lambda: imageself.saveResizedImage(self.combobox, self)
                                         )
-        self.submit.grid(row=2, column=0, padx=10, pady=5, sticky="ew")
+        self.submit.grid(row=2, column=0, padx=10, pady=5, sticky="ew", columnspan=4)
 
-        self.passButton=customtkinter.CTkButton(self, text="Pass", 
+        self.passButton=customtkinter.CTkButton(self, text="Pass", fg_color="orange", text_color="black",
                                         command=imageself.passMarking
                                         )
-        self.passButton.grid(row=2, column=1, padx=10, pady=5, sticky="ew")
+        self.passButton.grid(row=2, column=4, padx=10, pady=5, sticky="ew", columnspan=2)
         self.errLabel = customtkinter.CTkLabel(
             self, text="After select the frame, you have to click space or enter at keyboard.\nIf you want to remark the image please push the pass button!", text_color="red")
-        self.errLabel.grid(row=3, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
+        self.errLabel.grid(row=3, column=0, columnspan=6, padx=10, pady=5, sticky="ew")
 
         self.exitButton=customtkinter.CTkButton(self, text="Exit", fg_color="red",
                                                 command=lambda:self.exitFunction(imageself)
                                                 )
-        self.exitButton.grid(row=4, column=1, padx=10, pady=5, sticky="ew")
+        self.exitButton.grid(row=6, column=5, padx=10, pady=5, sticky="ew")
         
-        self.switch=customtkinter.CTkSwitch(self, text="Change Image", onvalue="on", offvalue="off",
-                                            command=lambda:self.changeImageFunc(imageself)
-                                            )
-        self.switch.grid(row=4, column=0, padx=10, pady=5, sticky="ew")
-    
+        self.addScreenPart(imageself)
+        # self.switch=customtkinter.CTkSwitch(self, text="Change Image", onvalue="on", offvalue="off",
+        #                                     command=lambda:self.changeImageFunc(imageself)
+        #                                     )
+        # self.switch.grid(row=4, column=0, padx=10, pady=5, sticky="ew")
+    def addScreenPart(self, imageself):
+        heightParts=imageself.getParts()[1]
+        widthParts=imageself.getParts()[0]
+        
+        self.heightVar = customtkinter.IntVar(self, 0)
+        self.widthVar = customtkinter.IntVar(self, 0)
+
+        rowNumber=4
+        columnNumber=0
+        customtkinter.CTkLabel(self, text="Height:", text_color="white").grid(row=rowNumber, column=columnNumber, padx=10, pady=5, sticky="ew")
+        columnNumber+=1
+        for heightPart in range(heightParts):
+            customtkinter.CTkRadioButton(master=self, text=f"{heightPart*self.winfo_screenheight()}-{(heightPart+1)*self.winfo_screenheight()}",
+                                            variable=self.heightVar, value=heightPart, command=lambda:imageself.getBaseImage(self)).grid(row=rowNumber, column=columnNumber, padx=2, pady=5, sticky="ew")
+            columnNumber+=1
+        
+        rowNumber+=1 
+        columnNumber=0
+        customtkinter.CTkLabel(self, text="Width:", text_color="white").grid(row=rowNumber, column=columnNumber, padx=10, pady=5, sticky="ew")
+        columnNumber+=1
+        for widthPart in range(widthParts):
+            customtkinter.CTkRadioButton(master=self, text=f"{widthPart*self.winfo_screenwidth()}-{(widthPart+1)*self.winfo_screenwidth()}",
+                                            variable=self.widthVar, value=widthPart, command=lambda:imageself.getBaseImage(self)).grid(row=rowNumber, column=columnNumber, padx=2, pady=5, sticky="ew")
+            columnNumber+=1
+
+            
+            
     def changeImageFunc(self, imageself):
         if not imageself.upsideORdownside:
             imageself.upsideORdownside=True
